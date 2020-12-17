a1b8fcdcd3c8db6e4b474215e6ab71b7c88c2f3c
Kamal <javadyadegar@gmail.com>
2020-12-17 03:34:36 +0330

    play with personal page

--- app/src/main/java/ir/hamedanmelk/hamedanmelk/ui/personalpage/MyHamedanMelkFragment.java
+++ app/src/main/java/ir/hamedanmelk/hamedanmelk/ui/personalpage/MyHamedanMelkFragment.java
@@ -15,24 +15,44 @@ public class MyHamedanMelkFragment extends PreferenceFragmentCompat {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        setPreferencesFromResource(R.xml.my_hamedan_melk_preference, rootKey);
-        Preference notification_button = findPreference("myhamedanmelk_notification");
-        Preference aboutpreference= findPreference("myhamedanmelk_aboutus_items");
         final NavController controller=Navigation.findNavController(Objects.requireNonNull(getActivity()),R.id.nav_host_fragment);
+        setPreferencesFromResource(R.xml.my_hamedan_melk_preference, rootKey);
+        // define preferences///////////////////////////////////////////////////
+        Preference about_preference= findPreference(getResources().getString(R.string.pref_screen_myhamedanmelk_aboutus_items));
+        Preference user_favorites = findPreference(getResources().getString(R.string.pref_screen_myhamedanmelk_user_favorites));
+        Preference user_lands     = findPreference("myhamedanmelk_user_lands");
+//        Preference register_company=findPreference(getResources().getString(R.string.pref_screen_myhamedanmelk_register_company));
 
-        Objects.requireNonNull(notification_button).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
-                return false;
-            }
-        });
+//        register_company.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+//            @Override
+//            public boolean onPreferenceClick(Preference preference) {
+//                controller.navigate(R.id.newCompanyFragment);
+//                return false;
+//            }
+//        });
 
-        Objects.requireNonNull(aboutpreference).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
-                controller.navigate(R.id.HelpFragment);
-                return false;
-            }
-        });
+//        user_lands.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+//            @Override
+//            public boolean onPreferenceClick(Preference preference) {
+//                controller.navigate(R.id.userLandFragment);
+//                return false;
+//            }
+//        });
+
+//        user_favorites.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+//            @Override
+//            public boolean onPreferenceClick(Preference preference) {
+//                controller.navigate(R.id.userFavoritesFragment);
+//                return false;
+//            }
+//        });
+
+//        Objects.requireNonNull(about_preference).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+//            @Override
+//            public boolean onPreferenceClick(Preference preference) {
+//                controller.navigate(R.id.HelpFragment);
+//                return false;
+//            }
+//        });
     }
 }
