74be57dfc81bc24800b989eef4efaf6a59c4e2a9
Kamal <javadyadegar@gmail.com>
2020-12-21 11:42:14 +0330

    update Offices UI Theme

--- app/src/main/java/ir/hamedanmelk/hamedanmelk/ui/office/OfficesRecyclerViewAdapter.java
+++ app/src/main/java/ir/hamedanmelk/hamedanmelk/ui/office/OfficesRecyclerViewAdapter.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,12 +12,18 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
+import java.util.Objects;
 
 import ir.hamedanmelk.hamedanmelk.R;
 import ir.hamedanmelk.hamedanmelk.models.OfficeModel;
+import ir.hamedanmelk.hamedanmelk.models.micro.CityModel;
+import ir.hamedanmelk.hamedanmelk.models.micro.DistrictModel;
+import ir.hamedanmelk.hamedanmelk.tools.Constants;
 import ir.hamedanmelk.hamedanmelk.tools.DownloadImage;
 import ir.hamedanmelk.hamedanmelk.tools.MYSQlDBHelper;
 import ir.hamedanmelk.hamedanmelk.tools.Urls;
@@ -47,13 +54,20 @@ public class OfficesRecyclerViewAdapter extends RecyclerView.Adapter<OfficesRecy
 
     @Override
     public void onBindViewHolder(final ViewHolder holder,final int position) {
+        MYSQlDBHelper qlDBHelper = new MYSQlDBHelper(activity.getApplicationContext());
         final OfficeModel officeModel = officeModels.get(position);
+        CityModel cityModel = qlDBHelper.GetCityByID(officeModel.getDistrict_id());
         holder.titleTxt.setText(officeModel.getTitle());
         holder.managerTxt.setText(officeModel.getManager());
         holder.addressTxt.setText(officeModel.getAddress());
         holder.phoneTxt.setText(officeModel.getPhone());
+        holder.noTxt.setText(officeModel.getNo());
+        holder.cityxt.setText(officeModel.getTitle());
         holder.faxTxt.setText(officeModel.getFax());
-        new DownloadImage(holder.thumbnailImg).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,Urls.getBaseURL()+"/"+officeModel.getLogo());
+
+        if(!officeModel.getLogo().equals("null")) {
+            new DownloadImage(holder.thumbnailImg).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, Urls.getBaseURL() + "/" + officeModel.getLogo());
+        }
         holder.phoneTxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -61,6 +75,18 @@ public class OfficesRecyclerViewAdapter extends RecyclerView.Adapter<OfficesRecy
                 Intent intent = new Intent(Intent.ACTION_DIAL);
                 intent.setData(Uri.parse(uri));
                 activity.startActivity(intent);
+            }
+        });
+
+
+        holder.shareTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("text/plain");
+                i.putExtra(Intent.EXTRA_SUBJECT, "اشتراک گذاری");
+                i.putExtra(Intent.EXTRA_TEXT, "https://hamedanmelk.ir/OfficeList");
+                activity.startActivity(Intent.createChooser(i, "اشتراک گذاری"));
             }
         });
 
@@ -76,8 +102,11 @@ public class OfficesRecyclerViewAdapter extends RecyclerView.Adapter<OfficesRecy
         public final TextView managerTxt;
         public final TextView addressTxt;
         public final TextView phoneTxt;
-        public final TextView faxTxt;
+        public final TextView noTxt;
+        public final TextView cityxt;
         public final ImageView thumbnailImg;
+        public final TextView shareTxt;
+        public final TextView faxTxt;
 
         public ViewHolder(View view) {
             super(view);
@@ -85,8 +114,11 @@ public class OfficesRecyclerViewAdapter extends RecyclerView.Adapter<OfficesRecy
             managerTxt = (TextView) view.findViewById(R.id.OfficeItemAdapterManagerTxt);
             addressTxt = (TextView)view.findViewById(R.id.OfficeItemAdapterAddressTxt);
             phoneTxt = (TextView)view.findViewById(R.id.OfficeItemAdapterPhoneTxt);
-            faxTxt = (TextView)view.findViewById(R.id.OfficeItemAdapterFaxTxt) ;
+            noTxt = (TextView)view.findViewById(R.id.OfficeItemAdapterNoTxt) ;
+            cityxt =(TextView)view.findViewById(R.id.OfficeItemAdapterCityTxt);
             thumbnailImg = (ImageView)view.findViewById(R.id.LawyerItemAdapterThumbnailImg);
+            shareTxt = (TextView)view.findViewById(R.id.OfficeItemAdapterShareTxt);
+            faxTxt = (TextView)view.findViewById(R.id.OfficeItemAdapterFaxTxt);
         }
     }
 }
