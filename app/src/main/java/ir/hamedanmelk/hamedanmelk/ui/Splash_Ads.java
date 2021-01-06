82599ff289b9c6b6871b07554e90ca85738478a9
Kamal <javadyadegar@gmail.com>
2021-01-06 03:40:18 +0330

    Change Logo Splash Screen

--- app/src/main/java/ir/hamedanmelk/hamedanmelk/ui/Splash_Ads.java
+++ app/src/main/java/ir/hamedanmelk/hamedanmelk/ui/Splash_Ads.java
@@ -42,47 +42,18 @@ public class Splash_Ads extends AppCompatActivity {
         ActionBar actionBar=getSupportActionBar();
         actionBar.hide();
         clicked=false;
-        final Handler handler = new Handler();
         LinearLayout linearLayout = (LinearLayout)findViewById(R.id.SplashAdsMainLeanerLayout);
-        final TextView numbertxt = (TextView)findViewById(R.id.numbercounter);
-        TextView skipbutton = (TextView)findViewById(R.id.splash_skip_text);
         TotalLandRequest(getApplicationContext());
-        linearLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                clicked=true;
-                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://hamedanmelk.ir/"));
-                startActivity(i);            }
-        });
-        skipbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                Intent i =new Intent(Splash_Ads.this, MainActivity.class);
-                startActivity(i);
-            }
-        });
-
-
-//        for(int i=4;i>=0;i--){
-//            numbertxt.setText(""+i);
-//            try {
-//                Thread.sleep(1000);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
-
-        handler.postDelayed(new Runnable(){
+        new Handler().postDelayed(new Runnable(){
             @Override
             public void run(){
-
                 Intent i =new Intent(Splash_Ads.this, MainActivity.class);
                 startActivity(i);
                 finish();
-                if(!clicked)handler.removeCallbacksAndMessages(null); // TODO this not work
             }
-        },100);
+        },750);
+
     }
 
 
